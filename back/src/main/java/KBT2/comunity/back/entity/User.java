eb829672bcc1e900950954c19d959a37ba44f965
gunhee <rjsgml771@naver.com>
2025-03-11 19:04:44 +0900

    feat: isDeleted에 따른 조회 제외 where 추가

--- back/src/main/java/KBT2/comunity/back/entity/User.java
+++ back/src/main/java/KBT2/comunity/back/entity/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.Where;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -15,6 +16,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Where(clause = "is_deleted = false")
 public class User {
     @Id
     @GeneratedValue
